b6cc4ea9286e030f8ce87bf007889e4f6a5cf30a
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-23 15:37:03 +0900

    [Feat] Book, Publisher 매핑관계 수정

--- src/main/java/com/springboot/relationship/domain/dto/BookDto.java
+++ src/main/java/com/springboot/relationship/domain/dto/BookDto.java
@@ -13,12 +13,14 @@ public class BookDto {
     private Long id;
     private String name;
     private String authorName;
+    private String publisherName;
 
     public static BookDto of (Book book) {
         return BookDto.builder()
                 .id(book.getId())
                 .name(book.getName())
                 .authorName(book.getAuthor().getName())
+                .publisherName(book.getPublisher().getName())
                 .build();
     }
 }
