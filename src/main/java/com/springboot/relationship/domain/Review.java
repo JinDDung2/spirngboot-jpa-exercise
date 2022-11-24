25a20073135d30993bf4a3f65cb1c0d914533134
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-24 13:06:36 +0900

    [Feat] Builder 추가

--- src/main/java/com/springboot/relationship/domain/Review.java
+++ src/main/java/com/springboot/relationship/domain/Review.java
@@ -1,12 +1,10 @@
 package com.springboot.relationship.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Generated;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
+@Builder
 @Entity
 @Getter
 @NoArgsConstructor
