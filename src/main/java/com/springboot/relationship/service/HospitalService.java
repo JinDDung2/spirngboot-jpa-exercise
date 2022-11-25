59c76a2f0c17f4226c1451777d4ad75e10737e89
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-25 15:00:37 +0900

    [Feat] findById 기능 추가

--- src/main/java/com/springboot/relationship/service/HospitalService.java
+++ src/main/java/com/springboot/relationship/service/HospitalService.java
@@ -1,6 +1,7 @@
 package com.springboot.relationship.service;
 
 import com.springboot.relationship.Repository.HospitalRepository;
+import com.springboot.relationship.domain.Hospital;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,11 @@ import org.springframework.stereotype.Service;
 public class HospitalService {
 
     private final HospitalRepository hospitalRepository;
+
+    public Hospital findById(Long id) {
+        Hospital hospital = hospitalRepository.findById(id).
+                orElseThrow(() -> new IllegalArgumentException("id가 없습니다."));
+        return hospital;
+    }
 
 }
